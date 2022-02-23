c0b4ee0ec027993570de3521d3946ec347a0ccdb
Felipe <fs6065155@gmail.com>
2022-02-23 20:38:31 -0300

    alterações

--- aula.js
+++ aula.js
@@ -1,5 +1,49 @@
 // aula de JS
 
-//IDE
- 
-//bha
+//variaveis
+
+// tipagem dinamica 
+var nome = "felipe"; //string > texto
+var idade = 12; //int > number
+var peso = 23.5; //float > number
+var andando = false; //bool >boolen < verdadeiro ou falso
+
+nome = "jair";
+
+//tipagem fraca
+nome = 143;
+
+//entrada > processamento >saida
+
+var n1 = 3
+var n2 = 7
+
+function soma(n1, n2)
+{
+    var total = n1 + n2;
+    return total;
+}
+soma(2,3);
+soma(n1,n2);
+
+function imprime(palavra)
+{
+    console.log(palavra);
+function abc ()
+{
+    console.log("abc")
+}
+ abc()
+}
+var div = function(n1, n2)
+{
+    return n1 / n2;
+}
+
+function calc (n1, n2, op)
+{
+    return op (n1, n2);
+}
+
+calc(3,2, div);
+calc(3,2, soma);
