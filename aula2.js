6ea4456abb6f40032e21395cd460f6c4daa9149d
Felipe <fs6065155@gmail.com>
2022-02-25 20:01:45 -0300

    alterações

--- aula2.js
+++ aula2.js
@@ -3,3 +3,16 @@
 // estrutura de dados
 
 var cidades = new Array("Curitiba", "Berlandia", "Berraba");
+
+//adiciona um item no vetor
+cidades.push('itapema');
+
+//alterar um item
+cidades [1] = 'Uberlandia';
+
+//permite adicionar um tipo diferente
+cidades.push = [23];
+
+//adicona iterm no meio do vetor
+
+cidades.splice (1, 0, "salvador");
